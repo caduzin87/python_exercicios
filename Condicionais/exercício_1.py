e0b952b46b6b65726458ddb7ddabc4f90d8787c3
caduzin87 <carlos.bafonifacincani@gmail.com>
2024-05-09 12:09:02 -0300

    Exercício_1.py

---
+++ Condicionais/exercício_1.py
@@ -0,0 +1,20 @@
+import json
+import urllib.request
+
+# Solicitar ao usuário o CEP
+cep = input('Digite o CEP: ')
+
+# Construir na URL de consulta
+url = f"http://viacep.com.br/ws/{cep}/json/"
+
+try:
+    # Fazer a requisição
+    response = urllib.request.urlopen(url)
+
+    # Ler o conteúdo da resposta
+    data = response.read().decode('utf-8')
+
+    # Teste...
+    print(data)
+except Exception as e:
+    print(f"Erro: {e}")

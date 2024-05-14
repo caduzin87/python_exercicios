8b5d4bb3201bfd3c4af97319a1ba72c6a10d385d
caduzin87 <carlos.bafonifacincani@gmail.com>
2024-05-14 09:26:02 -0300

    atualização_desafio.py

--- Condicionais/desafio.py
+++ Condicionais/desafio.py
@@ -7,3 +7,21 @@ tensao = resistencia * corrente
 
 # Exibe o resultado da tensão
 print("A tensão é:", tensao, "volts (V)")
+
+# Solicita ao usuário para inserir a tensao e a corrente
+tensao = float(input("Digite o valor da tensao em volts (V): "))
+corrente = float(input("Digite o valor da corrente em amperes (A): "))
+
+resistencia = tensao * corrente
+
+# Exibe o resultado da resistencia
+print("A resistencia é:", resistencia, "ohm (Ω)")
+
+# Solicita ao usuário para inserir a tensao e a resistencia
+tensao = float(input("Digite o valor da tensao em volts (V): "))
+resistencia = float(input("Digite o valor da resistencia em ohms(Ω): "))
+
+corrente = tensao * resistencia
+
+# Exibe o resultado da resistencia
+print("A corrente é:", corrente, "amperes (A)")
